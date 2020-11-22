cd6beb203b6da06c0af06e430f142bbe5997a434
Mike <miguel@dev.com>
2020-11-22 00:39:46 -0300

    + render recycler view with different cell types

--- app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Complex/ComplexUIActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Complex/ComplexUIActivity.kt
@@ -7,10 +7,15 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.LinearLayout
 import com.jonbott.learningrxjava.Common.disposedBy
+import com.jonbott.learningrxjava.Common.isEven
 import com.jonbott.learningrxjava.R
 import com.jonbott.learningrxjava.databinding.ActivityComplexUiBinding
 import com.jonbott.learningrxjava.databinding.ItemReactiveUiBinding
+import com.jonbott.learningrxjava.databinding.ItemReactiveUiOtherBinding
+import com.minimize.android.rxrecycleradapter.OnGetItemViewType
 import com.minimize.android.rxrecycleradapter.RxDataSource
+import com.minimize.android.rxrecycleradapter.RxDataSourceSectioned
+import com.minimize.android.rxrecycleradapter.ViewHolderInfo
 import io.reactivex.disposables.CompositeDisposable
 
 
@@ -34,7 +39,34 @@ class ReactiveUIActivity : Activity() {
         setContentView(R.layout.activity_complex_ui)
 
         commonInit()
-        showSimpleBindingExample()
+//        showSimpleBindingExample()
+        showComplexBindingExample()
+    }
+
+    private fun showComplexBindingExample() {
+        val viewHolderInfoList = listOf(
+                ViewHolderInfo(R.layout.item_reactive_ui, CellType.ITEM2.ordinal),
+                ViewHolderInfo(R.layout.item_reactive_ui_other, CellType.ITEM.ordinal)
+        )
+
+        val rxDataSourceSectioned = RxDataSourceSectioned(dataSet, viewHolderInfoList, object : OnGetItemViewType() {
+            override fun getItemViewType(position: Int): Int {
+                //determine cell type
+                return if (position.isEven) CellType.ITEM.ordinal
+                else CellType.ITEM2.ordinal
+            }
+        })
+
+        rxDataSourceSectioned.asObservable().subscribe {
+            val ui = it.viewDataBinding ?: return@subscribe
+            val data = it.item
+            when (ui) {
+                is ItemReactiveUiBinding -> ui.textViewItem.text = "Cell Type 1: $data"
+                is ItemReactiveUiOtherBinding -> ui.textViewItem2.text = "Cell Type 2: $data"
+            }
+        }.disposedBy(bag)
+
+        rxDataSourceSectioned.bindRecyclerView(boundActivity.reactiveUIRecyclerView)
     }
 
     private fun commonInit() {
